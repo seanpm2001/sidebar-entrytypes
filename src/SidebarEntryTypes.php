875e9f3785b618f44f18a0b76d496b73b960fd53
James Edmonston <james@ethercreative.co.uk>
2023-05-10 16:09:05 +0100

    Fix Craft 4 indexes not filtering correctly

--- src/SidebarEntryTypes.php
+++ src/SidebarEntryTypes.php
@@ -1,12 +1,12 @@
 <?php
 /**
-* Entry Types plugin for Craft CMS 4.x
-*
-* Easily switch between entry types in entries section
-*
-* @link      https://ethercreative.co.uk
-* @copyright Copyright (c) 2019 Ether Creative
-*/
+ * Entry Types plugin for Craft CMS 4.x
+ *
+ * Easily switch between entry types in entries section
+ *
+ * @link      https://ethercreative.co.uk
+ * @copyright Copyright (c) 2019 Ether Creative
+ */
 
 namespace ether\sidebarentrytypes;
 
@@ -14,87 +14,91 @@ use Craft;
 use craft\base\Plugin;
 use craft\base\Element;
 use craft\elements\Entry;
-use craft\models\Section;
 use craft\events\RegisterElementSourcesEvent;
-
+use craft\models\EntryType;
 use yii\base\Event;
 
 /**
-* Class SidebarEntryTypes
-*
-* @author    Ether Creative
-* @package   SidebarEntryTypes
-* @since     1.0.0
-*
-*/
+ * Class SidebarEntryTypes
+ *
+ * @author    Ether Creative
+ * @package   SidebarEntryTypes
+ * @since     1.0.0
+ *
+ */
 class SidebarEntryTypes extends Plugin
 {
-  // Static Properties
-  // =========================================================================
+    // Static Properties
+    // =========================================================================
 
-  /**
-  * @var SidebarEntryTypes
-  */
-  public static $plugin;
+    /**
+     * @var SidebarEntryTypes
+     */
+    public static $plugin;
 
-  // Public Properties
-  // =========================================================================
+    // Public Properties
+    // =========================================================================
 
-  /**
-  * @var string
-  */
-  public string $schemaVersion = '2.0.0';
+    /**
+     * @var string
+     */
+    public string $schemaVersion = "2.0.0";
 
-  // Public Methods
-  // =========================================================================
+    // Public Methods
+    // =========================================================================
 
-  /**
-  * @inheritdoc
-  */
-  public function init()
-  {
-    parent::init();
-    self::$plugin = $this;
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        self::$plugin = $this;
 
-    Event::on(Entry::class, Element::EVENT_REGISTER_SOURCES, function(RegisterElementSourcesEvent $event) {
-      $children = [];
+        Event::on(Entry::class, Element::EVENT_REGISTER_SOURCES, function (
+            RegisterElementSourcesEvent $event
+        ) {
+            $children = [];
 
-      foreach ($event->sources as $i => $source) {
-        if (!isset($source['data'])) {
-          continue;
-        }
+            foreach ($event->sources as $i => $source) {
+                if (!isset($source["data"])) {
+                    continue;
+                }
 
-        $entryTypes = Craft::$app->sections->getEntryTypesBySectionId($source['criteria']['sectionId']);
+                /** @var EntryType[] */
+                $entryTypes = Craft::$app->sections->getEntryTypesBySectionId(
+                    $source["criteria"]["sectionId"]
+                );
 
-        if (count($entryTypes) < 2) {
-          continue;
-        }
+                if (count($entryTypes) < 2) {
+                    continue;
+                }
 
-        $children[$i] = [];
+                $children[$i] = [];
 
-        foreach ($entryTypes as $entryType) {
-          $children[$i][] = [
-            'key' => 'section:' . $entryType->uid,
-            'label' => $entryType->name,
-            'data' => [
-              'has-structure' => true,
-              'default-sort' => 'structure:asc',
-              'type' => 'structure',
-              'handle' => $entryType->handle,
-              'entry-type' => true
-            ],
-            'criteria' => [
-              'sectionId' => $entryType->sectionId,
-              'type' => $entryType->handle,
-              'editable' => false,
-              ]
-            ];
-          }
-        }
+                foreach ($entryTypes as $entryType) {
+                    $section = $entryType->getSection();
 
-        foreach ($children as $key => $child) {
-          $event->sources[$key]['nested'] = $child;
-        }
-      });
+                    $children[$i][] = [
+                        "key" => "section:" . $entryType->uid,
+                        "label" => $entryType->name,
+                        "data" => [
+                            "type" => "structure",
+                            "handle" => $section->handle,
+                        ],
+                        "criteria" => [
+                            "sectionId" => $entryType->sectionId,
+                            "typeId" => $entryType->id,
+                            "editable" => true,
+                        ],
+                        "defaultSort" => ["structure", "asc"],
+                    ];
+                }
+            }
+
+            foreach ($children as $key => $child) {
+                $event->sources[$key]["nested"] = $child;
+            }
+        });
     }
-  }
+}
